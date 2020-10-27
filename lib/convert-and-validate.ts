8fc7b2862ea99c584555ec269877b53e88ee5647
Ruben v. Rooij <ruben.van.rooij@gmail.com>
2020-10-27 14:03:15 +0100

    initial

--- lib/convert-and-validate.ts
+++ lib/convert-and-validate.ts
@@ -1,20 +1,30 @@
-import { ValidateOptions } from './models';
+import { ValidateOptions, IErrorDetail } from './models';
 import { plainToClass } from 'class-transformer';
-import { validate, ValidatorOptions } from 'class-validator';
+import { validate, ValidatorOptions, ValidationError } from 'class-validator';
 import { BAD_REQUEST } from 'http-status-codes';
 import { HttpError } from './http-error';
 
 const defaultValidatorOptions: ValidatorOptions = { forbidNonWhitelisted: true, whitelist: true };
+
+function extractErrorMessages(errors: ValidationError[], parentProperty?: string): IErrorDetail[] {
+    return errors.reduce((prev, error) => {
+        const property = parentProperty ? `${parentProperty}.${error.property}` : error.property;
+        const constraints = error.constraints ? [{
+            message: Object.values(error.constraints),
+            name: property,
+        }] : [];
+        const childErrors = error.children ? extractErrorMessages(error.children, property) : [];
+
+        return [...prev, ...constraints, ...childErrors];
+    }, new Array<IErrorDetail>());
+}
 
 export async function convertAndValidate<T>(plain: any, options: ValidateOptions<T>, errorMessage: string): Promise<T> {
     const value = plainToClass(options.classType, plain);
     const errors = await validate(value, options.options || defaultValidatorOptions);
 
     if (errors.length > 0) {
-        throw new HttpError(BAD_REQUEST, errorMessage, ...errors.map((error) => ({
-            message: Object.values(error.constraints),
-            name: error.property
-        })));
+        throw new HttpError(BAD_REQUEST, errorMessage, ...extractErrorMessages(errors));
     }
 
     return value;
